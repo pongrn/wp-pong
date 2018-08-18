dede2c40c7109a86f9d66a416dc9eb6807ddbb2b
Giancarlo Silva <giancarlozero@gmail.com>
2018-08-18 00:48:15 -0300

    Estrutura da home concluída.

---
+++ js/init.js
@@ -0,0 +1,40 @@
+// Foundation
+$(document).foundation();
+
+// Slick
+$(document).ready(function(){
+    // Slideshow de logotipos
+    $('.slideshow-logos').slick({
+        autoplay: true,
+        autoplaySpeed: 5000,
+        arrows: true,
+        cssEase: 'ease',
+        dots: false,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        centerMode: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 640,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
