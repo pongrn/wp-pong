04f31542b18a2e13e17f35e32e626b70486dd7d1
Giancarlo Silva <giancarlozero@gmail.com>
2018-08-31 17:38:00 -0300

    Exibição de post implementada.

--- single.php
+++ single.php
@@ -8,28 +8,28 @@
 
         <div class="row">
             <article class="posts-wrapper bg-claro small-11 small-centered medium-8 medium-uncentered large-8 large-uncentered column">
-                <div class="post-titulo text-center">
-                    <h1>Lorem Ipsum</h1>
-                    Autor(a): <a href="#">Fulano de tal</a> - 12/34/5678 - <a href="#">XX comentários</a><br>
-                    Tempo estimado de leitura: XX minutos
-                </div>
+                <?php if (have_posts()) : ?>
+                    <?php while (have_posts()) : the_post(); ?>
+                        <div class="post-titulo text-center">
+                            <?php the_title('<h1>', '</h1>'); ?>
+                            <?php the_time('j \d\e F \d\e Y'); ?> - <?php the_author_posts_link(); ?> - <?php comments_popup_link('Nenhum comentário', '1 comentário', '% comentários', 'comments-link', ''); ?><br>
+                            Tempo estimado de leitura: XX minutos
+                        </div>
 
-                <div class="post-texto">
-                    <h1>Lorem ipsum</h1>
-                    <h2>Lorem ipsum</h2>
-                    <h3>Lorem ipsum</h3>
-                    <h4>Lorem ipsum</h4>
-                    <h5>Lorem ipsum</h5>
-                    <h6>Lorem ipsum</h6>
+                        <div class="post-texto">
+                            <?php the_content(); ?>
+                        </div>
 
-                    <p>Magna <a href="#">et adipisicing eu tempor consequat</a>. Dolore sit <b>consectetur quis est proident sint</b> pariatur consectetur irure deserunt magna tempor. Ullamco sit voluptate do ad ea culpa non labore mollit mollit ex sint aliqua.</p>
-
-                    <p><i>Culpa non elit labore qui Lorem fugiat sint qui in in excepteur magna.</i> Sint ullamco labore laborum anim. Veniam do incididunt veniam laboris tempor nulla est voluptate laboris. Cupidatat aliqua voluptate Lorem Lorem quis. Et est consectetur commodo cillum mollit enim ut adipisicing sunt. Consectetur amet amet duis voluptate ipsum labore non ullamco. Reprehenderit qui incididunt nulla elit laboris laboris pariatur ex magna amet voluptate commodo ullamco qui.</p>
-                </div>
-
-                <div class="post-comentarios">
-                    Caixa de comentários aqui.
-                </div>
+                        <div class="post-comentarios">
+                            <?php comments_template(); ?>
+                        </div>
+                    <?php endwhile; ?>
+                <?php else : ?>
+                    <div class="post-404 post-titulo text-center">
+                        <h1>404 - Not Found</h1>
+                        <p>Post não encontrado.</p>
+                    </div>
+                <?php endif; ?>
             </article>
             <?php get_sidebar(); ?>
         </div>
