c10d307e90bdafcadc7cbc9e70e0a1d46e9f3617
Giancarlo Silva <giancarlozero@gmail.com>
2018-08-31 17:39:08 -0300

    Exibição de página estática implementada.

--- page.php
+++ page.php
@@ -8,22 +8,22 @@
 
         <div class="row">
             <article class="paginas-wrapper bg-claro small-11 small-centered medium-12 medium-uncentered large-12 large-uncentered column">
-                <div class="pagina-titulo text-center">
-                    <h1>Lorem Ipsum</h1>
-                </div>
+                <?php if (have_posts()) : ?>
+                    <?php while (have_posts()) : the_post(); ?>
+                        <div class="pagina-titulo text-center">
+                            <?php the_title('<h1>', '</h1>'); ?>
+                        </div>
 
-                <div class="pagina-texto">
-                    <h1>Lorem ipsum</h1>
-                    <h2>Lorem ipsum</h2>
-                    <h3>Lorem ipsum</h3>
-                    <h4>Lorem ipsum</h4>
-                    <h5>Lorem ipsum</h5>
-                    <h6>Lorem ipsum</h6>
-
-                    <p>Magna <a href="#">et adipisicing eu tempor consequat</a>. Dolore sit <b>consectetur quis est proident sint</b> pariatur consectetur irure deserunt magna tempor. Ullamco sit voluptate do ad ea culpa non labore mollit mollit ex sint aliqua.</p>
-
-                    <p><i>Culpa non elit labore qui Lorem fugiat sint qui in in excepteur magna.</i> Sint ullamco labore laborum anim. Veniam do incididunt veniam laboris tempor nulla est voluptate laboris. Cupidatat aliqua voluptate Lorem Lorem quis. Et est consectetur commodo cillum mollit enim ut adipisicing sunt. Consectetur amet amet duis voluptate ipsum labore non ullamco. Reprehenderit qui incididunt nulla elit laboris laboris pariatur ex magna amet voluptate commodo ullamco qui.</p>
-                </div>
+                        <div class="pagina-texto">
+                            <?php the_content(); ?>
+                        </div>
+                    <?php endwhile; ?>
+                <?php else : ?>
+                    <div class="post-404 pagina-titulo text-center">
+                        <h1>404 - Not Found</h1>
+                        <p>Post não encontrado.</p>
+                    </div>
+                <?php endif; ?>
             </article>
         </div>
     </section>
