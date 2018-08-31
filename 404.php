05c0ff5f7d3565e43275d5bf99e3cf96f5b8d54a
Giancarlo Silva <giancarlozero@gmail.com>
2018-08-31 17:38:49 -0300

    Página de erro 404 implementada.

---
+++ 404.php
@@ -0,0 +1,18 @@
+<?php get_header(); ?>
+    <section class="conteudo-pagina">
+        <div class="row">
+            <div class="pong-breadcrumbs small-12 medium-12 large-12 column text-right">
+                Início / <a href="#">Lorem ipsum</a>
+            </div>
+        </div>
+
+        <div class="row">
+            <article class="paginas-wrapper bg-claro small-11 small-centered medium-12 medium-uncentered large-12 large-uncentered column">
+                <div class="post-404 pagina-titulo text-center">
+                    <h1>404 - Not Found</h1>
+                    <p>Post não encontrado.</p>
+                </div>
+            </article>
+        </div>
+    </section>
+<?php get_footer(); ?>
